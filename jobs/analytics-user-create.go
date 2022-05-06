968875fb7ab0a02b00d0ccdd3edd13af09cc0398
Allen Thomas <allen.thomas@harness.io>
2022-05-06 13:34:17 -0400

    [GTM-919]: Modified queries to access db

--- jobs/analytics-user-create.go
+++ jobs/analytics-user-create.go
@@ -3,6 +3,8 @@ package jobs
 import (
 	"context"
 	"github.com/sirupsen/logrus"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"gopkg.in/segmentio/analytics-go.v3"
 	"plg-utilities/core"
 	"plg-utilities/db/mongodb"
@@ -10,6 +12,11 @@ import (
 	"sync"
 	"time"
 )
+
+type MinimalAccount struct {
+	Id          string `json:"_id,omitempty" bson:"_id,omitempty"`
+	AccountName string `json:"accountName,omitempty" bson:"accountName,omitempty"`
+}
 
 func runAnalyticsUserCreate(mongo *mongodb.MongoDb, segmentSender *segment.HTTPClient) error {
 	ctx := context.Background()
@@ -34,68 +41,104 @@ func runAnalyticsUserCreate(mongo *mongodb.MongoDb, segmentSender *segment.HTTPC
 		}(segmentSender, &wg)
 	}
 
-	// cache to track accountID to account Name
-	// this is useful for user Group Event
-	accountIdToName := map[string]string{}
+	// get all account Ids
+	var accounts []MinimalAccount
 
-	// process every account
 	collectionName := mongo.AccountDAO.AccountCollection.Name()
-	accountCursor, err := mongo.AccountDAO.ListWithCursor(ctx)
+	opts := options.Find().SetProjection(bson.D{{"_id", 1}, {"accountName", 1}})
+	accountCursor, err := mongo.AccountDAO.AccountCollection.Find(ctx, bson.D{}, opts)
 	if err != nil {
-		logrus.Fatalf("unable to list collection %s: %s", collectionName, err.Error())
+		logrus.Errorf("unable to find collection %s: %s", collectionName, err.Error())
 	}
-	defer accountCursor.Close(ctx)
-	for accountCursor.Next(ctx) {
-		var account core.Account
-		err := accountCursor.Decode(&account)
-		if err != nil {
-			logrus.Errorf("unable to decode record for collection %s with name %s: %s", collectionName, account.AccountName, err.Error())
-		}
-		// add to cache
-		accountIdToName[account.Id] = account.AccountName
+	if err := accountCursor.All(ctx, &accounts); err != nil {
+		logrus.Errorf("unable to list collection %s: %s", collectionName, err.Error())
+	}
+	accountCursor.Close(ctx)
+
+	// process every account and every use for that account
+	collectionName = mongo.UserDAO.UserCollections.Name()
+	for _, account := range accounts {
+		// account telemetry
 		createAccountIdentityEvent(account, &batchEvents, batchEventsQueue)
 		createAccountGroupEvent(account, &batchEvents, batchEventsQueue)
-	}
-
-	if err := accountCursor.Err(); err != nil {
-		logrus.Errorf("unable to list entire collection %s: %s", collectionName, err.Error())
-	}
-
-	//process every user
-	collectionName = mongo.UserDAO.UserCollections.Name()
-	userCursor, err := mongo.UserDAO.ListWithCursor(ctx)
-	if err != nil {
-		logrus.Fatalf("unable to list collection %s: %s", collectionName, err.Error())
-	}
-	for userCursor.Next(ctx) {
-		var user core.User
-		err := userCursor.Decode(&user)
+		// user telemetry
+		userCurser, err := mongo.UserDAO.UserCollections.Find(ctx, bson.M{"accounts": account.Id})
 		if err != nil {
-			logrus.Errorf("unable to decode record for collection %s with name %s: %s", collectionName, user.Name, err.Error())
+			logrus.Errorf("failed to find users for account %s: %s", account.Id, err.Error())
 		}
-		createUserIdentityEvent(user, &batchEvents, batchEventsQueue)
-		for _, accountId := range user.Accounts {
-			createUserGroupEvent(user, accountId, accountIdToName[accountId], &batchEvents, batchEventsQueue)
+		var users []core.User
+		if err := userCurser.All(ctx, &users); err != nil {
+			logrus.Errorf("failed to list users for account %s: %s", account.Id, err.Error())
+		}
+		userCurser.Close(ctx)
+		for _, user := range users {
+			createUserIdentityEvent(user, &batchEvents, batchEventsQueue)
+			createUserGroupEvent(user, account.Id, account.AccountName, &batchEvents, batchEventsQueue)
 		}
 	}
 
-	// flush batch events if there is any left
-	if len(batchEvents) != 0 {
-		flush(&batchEvents, batchEventsQueue)
-	}
-
-	if err := userCursor.Err(); err != nil {
-		logrus.Errorf("unable to list entire collection %s: %s", collectionName, err.Error())
-	}
-
-	defer userCursor.Close(ctx)
+	//// cache to track accountID to account Name
+	//// this is useful for user Group Event
+	//accountIdToName := map[string]string{}
+	//
+	//// process every account
+	//collectionName := mongo.AccountDAO.AccountCollection.Name()
+	//accountCursor, err := mongo.AccountDAO.ListWithCursor(ctx)
+	//if err != nil {
+	//	logrus.Fatalf("unable to list collection %s: %s", collectionName, err.Error())
+	//}
+	//defer accountCursor.Close(ctx)
+	//for accountCursor.Next(ctx) {
+	//	var account core.Account
+	//	err := accountCursor.Decode(&account)
+	//	if err != nil {
+	//		logrus.Errorf("unable to decode record for collection %s with name %s: %s", collectionName, account.AccountName, err.Error())
+	//	}
+	//	// add to cache
+	//	accountIdToName[account.Id] = account.AccountName
+	//	createAccountIdentityEvent(account, &batchEvents, batchEventsQueue)
+	//	createAccountGroupEvent(account, &batchEvents, batchEventsQueue)
+	//}
+	//
+	//if err := accountCursor.Err(); err != nil {
+	//	logrus.Errorf("unable to list entire collection %s: %s", collectionName, err.Error())
+	//}
+	//
+	////process every user
+	//collectionName = mongo.UserDAO.UserCollections.Name()
+	//userCursor, err := mongo.UserDAO.ListWithCursor(ctx)
+	//if err != nil {
+	//	logrus.Fatalf("unable to list collection %s: %s", collectionName, err.Error())
+	//}
+	//for userCursor.Next(ctx) {
+	//	var user core.User
+	//	err := userCursor.Decode(&user)
+	//	if err != nil {
+	//		logrus.Errorf("unable to decode record for collection %s with name %s: %s", collectionName, user.Name, err.Error())
+	//	}
+	//	createUserIdentityEvent(user, &batchEvents, batchEventsQueue)
+	//	for _, accountId := range user.Accounts {
+	//		createUserGroupEvent(user, accountId, accountIdToName[accountId], &batchEvents, batchEventsQueue)
+	//	}
+	//}
+	//
+	//// flush batch events if there is any left
+	//if len(batchEvents) != 0 {
+	//	flush(&batchEvents, batchEventsQueue)
+	//}
+	//
+	//if err := userCursor.Err(); err != nil {
+	//	logrus.Errorf("unable to list entire collection %s: %s", collectionName, err.Error())
+	//}
+	//
+	//defer userCursor.Close(ctx)
 
 	close(batchEventsQueue)
 	wg.Wait()
-	return err
+	return nil
 }
 
-func createAccountIdentityEvent(account core.Account, batchEvents *[]analytics.Message, queue chan []analytics.Message) {
+func createAccountIdentityEvent(account MinimalAccount, batchEvents *[]analytics.Message, queue chan []analytics.Message) {
 	event := analytics.Identify{
 		UserId:       segment.ACCOUNT_ANALYSIS_USER_PREFIX + account.Id,
 		Timestamp:    time.Now(),
@@ -106,7 +149,7 @@ func createAccountIdentityEvent(account core.Account, batchEvents *[]analytics.M
 	flushIfLimit(batchEvents, queue)
 }
 
-func createAccountGroupEvent(account core.Account, batchEvents *[]analytics.Message, queue chan []analytics.Message) {
+func createAccountGroupEvent(account MinimalAccount, batchEvents *[]analytics.Message, queue chan []analytics.Message) {
 	event := analytics.Group{
 		UserId:       segment.ACCOUNT_ANALYSIS_USER_PREFIX + account.Id,
 		GroupId:      account.Id,
